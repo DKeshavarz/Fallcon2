9608f315aeb456a687e1395188f842304186ae78
DKeshavarz <dankeshavarz1075@gmail.com>
2024-06-27 18:02:04 +0330

    chore : add special movec for class space object

--- spaceObject.cpp
+++ spaceObject.cpp
@@ -24,15 +24,35 @@ bool SpaceObject::canCollision(const Point& loacation)const{
     return false;
 }
 const vector<Point> SpaceObject::creatFromMap (Point loacation,const vector<vector<int>>& map){
+    vector<Point> ans;
     //needs better validation
-    const vector<Point> directions {{0,0}/*stay*/,{1,0}/*down*/,{0,1}/*right*/,{1,1}/*down right*/};
-    
-    for(const auto& dir : directions){
+    const vector<Point> planetDirections {{0,0}/*stay*/,{1,0}/*down*/,{0,1}/*right*/,{1,1}/*down right*/};
+    const vector<Point> enteryDirection  {{2,0}/*down*/,{2,1}/*down*/,{1,2}/*right*/,{0,2}/*right*/
+                                         ,{-1,1}/*up*/,{-1,0}/*up*/ ,{0,-1}/*left*/,{1,-1}/*left*/};
+
+    for(const auto& dir : planetDirections){
         this->planet.push_back(dir+loacation);
+        ans.push_back(dir+loacation);
+    }
+    for(const auto& dir : enteryDirection){
+        this->getEntries().push_back(dir+loacation);
+        ans.push_back(dir+loacation);
     }
 
-    return {this->planet};
+    return ans;
 }
-const vector<Point> SpaceObject::specialEffect(Point)const{
+const vector<Point> SpaceObject::specialEffect(Point loacation)const{
+    if(!this->isEntry(loacation)){
+        throw invalid_argument("can not use spaceobject here");
+    }
 
+    int i {};
+    vector<Point> way;
+    while(this->constGetEntries().at(i) != loacation)++i;
+
+    for(int j {0} ;  j < 4 ; ++j){
+        way.push_back(this->constGetEntries().at((i+j)%constGetEntries().size()));
+    }
+
+    return way;
 }
