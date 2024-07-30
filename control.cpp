64843130eac4045985259e98e650989b892b8ebb
Kian81-S <telegramkian@gmail.com>
2024-07-30 13:24:07 +0330

    Update control.cpp

--- control.cpp
+++ control.cpp
@@ -31,8 +31,29 @@ int Control::run(){
         }catch(invalid_argument& err){
             this->io.output(err.what() + string(1,'\n'));
         }
+                Event event;
+        while (window.pollEvent(event))
+        {
+           if (event.type == Event::Closed)
+              window.close();
+
+           if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape)
+              window.close();
+        }
+
+        // Clear screen
+        window.clear();
+
+        // Draw the sprites
+        for (const auto& sprite : sprites)
+        {
+           window.draw(sprite);
+        }
+
+        // Update the window
+        window.display();
         
-    }while(ch != 'q');
+    }while(ch != 'q' && window.isOpen());
     
     return 0;
 }
